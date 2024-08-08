22e4f9896bff6ee3145afd778891512fd20b0ae3
nika mdinaradze <mdinaradzenika04@gmail.com>
2024-08-08 19:55:24 +0400

    add operators

---
+++ nikoloz_mdinaradze_lecture/operators.py
@@ -0,0 +1,36 @@
+num1 = 1
+num2 = 2
+print(num1 + num2)
+print(num1 - num2)
+
+num1 = 1
+num2 = 1.5
+print(num1 + num2)
+print(num1 - num2)
+print(num1 / num2)
+print(num1 * num2)
+
+str1 = "abc"
+num1 = 5
+print(str1 * num1)
+
+str1 = "abc"
+str2 = "dfg"
+print(str1+str2)
+
+list1 = [1,2,3,4]
+int1 = 2
+print(list1*int1)
+
+tuple1 = (1,2,3,4)
+print(tuple1*int1)
+
+str1 = "abcd"
+
+str1 = str1 + "1"
+str1 += "1"
+print(str1)
+
+int1 = 1
+int1 -= 2
+print(int1)
